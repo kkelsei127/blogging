031844ff41a934cb1f7d43c3f24530ce61f282ed
Kelsei Kyser <kkelsei127@yahoo.com>
2023-02-18 19:18:46 -0500

    added seed data

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { user, post, comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await user.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
